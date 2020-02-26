3a9fb6e87ada92d079c14c94dc1f554d6ad44652
Likhitha74 <60224506+Likhitha74@users.noreply.github.com>
2020-02-26 21:26:16 +0530

    Add files via upload

---
+++ LoggingTask/HouseConstructionCost/ConstructionCostMain.java
@@ -0,0 +1,31 @@
+package cleancode.houseconstruction;
+
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ConstructionCostMain
+{
+	private static final Logger LOGGER= LogManager.getLogger(Main.class);
+	
+	public static void main(String [] args)
+	{
+		ConstructionCost obj=new ConstructionCost();
+		int typeofhouse;
+		double totalarea;
+		Scanner sc=new Scanner(System.in);
+		LOGGER.info("Available materials types\n");
+		//Type of House is ... "1.Standard Material 2.Above Standard Material 3.High Standard Material 4.High Standard Material and Fully Automated Home"
+		LOGGER.info("1.Standard materials\t2.Above standard materials\t3.High Standard material\t4.High Standard Material and Fully Automated Home");
+		LOGGER.info("Choose a material type:");
+		typeofhouse=sc.nextInt();
+		if typeofhouse>4
+		{
+			LOGGER.info("Invalid choice\n");
+			return;
+		}
+		LOGGER.info("Enter area : ");
+		totalarea = sc.nextDouble();
+		LOGGER.info("Cost of construction is : "+obj.ConstructionCost(totalarea,typeofhouse)+"\n");
+	}
+}
